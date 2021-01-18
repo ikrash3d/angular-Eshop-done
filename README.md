32d529f034a6304007443e0974fadd35fa8e2c95
ikrash3d <samycheklat@hotmail.fr>
2021-01-17 20:55:03 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-cwsibu-jsx7kp
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-cwsibu-jsx7kp)
---
+++ src/app/products.ts
@@ -0,0 +1,38 @@
+export const products = [
+  {
+    id: 1,
+    name: "Phone XL",
+    price: 799,
+    description: "A large phone with one of the best screens"
+  },
+  {
+    id: 2,
+    name: "Phone Mini",
+    price: 699,
+    description: "A great phone with one of the best cameras"
+  },
+  {
+    id: 3,
+    name: "Phone Standard",
+    price: 299,
+    description: ""
+  },
+  {
+    id: 4,
+    name: "Samsung Galaxy S21",
+    price: 1399,
+    description: "You will need to sell a kidney for it"
+  },
+  {
+    id: 5,
+    name: "Iphone 12 XL+",
+    price: 2399,
+    description: "You will need to sell two kidneys for it"
+  }
+];
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
